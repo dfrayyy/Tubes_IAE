38cd8b5756a3952bb483803cb84fc4f65f66186f
farhanathallah <148053269+farhanathallah@users.noreply.github.com>
2025-06-13 23:00:28 +0700

    Update app.py

--- Stock in-service/app.py
+++ Stock in-service/app.py
@@ -4,16 +4,12 @@ from database import db_session, init_db
 from schema import schema
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/inventory'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# GraphQL endpoint
 app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view(
         'graphql',
         schema=schema,
-        graphiql=True  # Enable GraphiQL interface
+        graphiql=True
     )
 )
 
@@ -23,4 +19,4 @@ def shutdown_session(exception=None):
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True) 
+    app.run(host='0.0.0.0', port=5002, debug=True)
