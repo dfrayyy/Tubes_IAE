9649712c317b38fabb25e72b4da0b563713ca075
farhanathallah <athallahfarhan6@gmail.com>
2025-06-13 09:45:13 +0700

    pushcode

---
+++ Stock in-service/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine('postgresql://postgres:postgres@db:5432/inventory')
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engine)
